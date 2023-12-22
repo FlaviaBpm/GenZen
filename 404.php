6682c956aabd7719b65005f9e7f0a63e5ae2495e
FlaviaBpm <flaviakaren12@gmail.com>
2023-12-22 15:06:31 +0100

    fin

--- 404.php
+++ 404.php
@@ -1,44 +1,14 @@
+
+<link href="<?php echo get_template_directory_uri(); ?>/page.css" rel="stylesheet"/>
+
 <?php get_header(); ?>
-   
-
-
-   <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            background-color: #f5f5f5;
-            color: #red;
-            text-align: center;
-            padding: 50px;
-        }
-
-
-        p {
-            font-size: 1.2em;
-            margin-top: 20px;
-            color: #555;
-        }
-
-        img {
-            max-width: 100%;
-            height: auto;
-            margin-top: 20px;
-        }
-
-        a {
-            color: red;
-            text-decoration: none;
-            font-weight: bold;
-        }
-
-        a:hover {
-            text-decoration: underline;
-        }
-    </style>
+   <div class="error">
+    
     <h1>Erreur 404 - Page non trouvée</h1>
     <p>Nous sommes désolés, mais la page que vous recherchez semble introuvable.</p>
     <p>Il est important de prendre soin de votre santé mentale. <br> Si vous avez besoin d'aide, n'hésitez pas à contacter <a href="https://www.exemple-aide-sante-mentale.com" target="_blank">notre service d'assistance</a>.</p>
-    <img src="lien-vers-votre-image.jpg" alt="Image illustrative">
-
+    <img src="<?php echo get_template_directory_uri();?>\assets\img\depression.png" alt="Image illustrative">
+</div>
 
 
 
